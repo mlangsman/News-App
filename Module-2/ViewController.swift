1d25f1fed01939a9f4c25b44b4448f79e33279ef
Marc <marc@langsman.com>
2019-09-30 13:44:46 +0100

    Initial Commit

---
+++ Module-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Module-2
+//
+//  Created by Marc Langsman on 30/09/2019.
+//  Copyright © 2019 Marc Langsman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
